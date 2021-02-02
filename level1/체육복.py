db3c0eda659aecb30d74de040748bfadefbdb7a8
Ahyoung <ay91911@gmail.com>
2021-02-02 17:15:56 +0900

    2021-02-02

---
+++ level1/체육복.py
@@ -0,0 +1,22 @@
+n=5
+lost=[2,4]
+reserve=[1,3,5]
+
+def solution(n, lost, reserve):
+    for i in range(1,n+1) :
+        if i in lost and i in reserve:
+            reserve.remove(i)
+            lost.remove(i)
+
+    for i in range(1, n+1):
+        if i in lost :
+            if (i - 1) in reserve :
+                reserve.remove(i - 1)
+                lost.remove(i)
+            elif (i + 1) in reserve :
+                reserve.remove(i + 1)
+                lost.remove(i)
+
+    return n - len(lost)
+
+print(solution(n,lost,reserve))
